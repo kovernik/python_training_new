81455ae76a4bc9685538ef3c4ec8a179083e4e45
motekito <kovernik@sofbalance.ru>
2017-10-11 14:35:54 +0300

    15 задание

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,23 @@
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", lastname="", email="", mobilephone="")] + [
+    Contact(firstname=random_string("Name", 10), lastname=random_string("Last name", 10),
+            email=random_string("kovernik@softbalance.ru", 10), mobilephone=random_string("+79110000000", 10))
+    for i in range(3)]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Name", lastname="Last name", email="kovernik@softbalance.ru",
-                      mobilephone="+79110000000", homephone="+78120001110", workphone="+91121111111",
-                      company="SoftBalance", address="Shaumyana, 55", middle="Middle", nickname="nickname")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()

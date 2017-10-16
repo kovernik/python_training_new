6eda9a4dde2701df895954e5125fd3a009c9535f
motekito <motekito@gmail.com>
2017-10-16 20:51:46 +0300

    6_4 contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,18 +1,6 @@
 from model.contact import Contact
 import pytest
-from data.add_contact import constant as testdata
-
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", lastname="", email="", mobilephone="")] + [
-    Contact(firstname=random_string("Name", 10), lastname=random_string("Last name", 10),
-            email=random_string("kovernik@softbalance.ru", 10), mobilephone=random_string("+79110000000", 10))
-    for i in range(3)]
+from data.add_contacts import constant as testdata
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])

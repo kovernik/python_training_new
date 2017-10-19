92a6bbe660a8b21ea9f40eed7c8e2a1896b35ce2
motekito <kovernik@sofbalance.ru>
2017-10-19 18:28:34 +0300

    7_1

---
+++ data/contacts.py
@@ -0,0 +1,8 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="firstname1", lastname="lastname1", email="email1"),
+    Contact(firstname="firstname2", lastname="lastname2", email="email2")
+]
+
--- generator/contact.py
+++ generator/contact.py
@@ -1,11 +1,10 @@
-from model.group import Group
+from model.contact import Contact
 import random
 import string
 import os.path
 import jsonpickle
 import getopt
 import sys
-
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
@@ -14,7 +13,7 @@ except getopt.GetoptError as err:
     sys.exit(2)
 
 n = 5
-f = "data/groups.json"
+f = "data/contacts.json"
 
 for o, a in opts:
     if o == "-n":
@@ -24,14 +23,15 @@ for o, a in opts:
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(n)
-]
+testdata = [Contact(firstname="", lastname="", email="", mobilephone="")] + [
+    Contact(firstname=random_string("Name", 10), lastname=random_string("Last name", 10),
+            email=random_string("kovernik@softbalance.ru", 10), mobilephone=random_string("+79110000000", 10))
+    for i in range(n)]
+
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 

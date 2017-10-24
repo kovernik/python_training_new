9ca94457e4ead258723e813ccec6c441af99aaed
motekito <kovernik@sofbalance.ru>
2017-10-24 13:23:50 +0300

    Слияние работа

--- data/contacts.py
+++ data/contacts.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from model.contact import Contact
 
 
@@ -7,13 +6,3 @@ testdata = [
     Contact(firstname="firstname2", lastname="lastname2", email="email2")
 ]
 
-=======
-from model.contact import Contact
-
-
-testdata = [
-    Contact(firstname="firstname1", lastname="lastname1", email="email1"),
-    Contact(firstname="firstname2", lastname="lastname2", email="email2")
-]
-
->>>>>>> c700b9504d602a9735d9791a0b69084b0d72be79

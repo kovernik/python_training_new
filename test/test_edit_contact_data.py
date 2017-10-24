91819225a9449fdbf9dba956285cce47c45f1de2
motekito <kovernik@sofbalance.ru>
2017-10-24 18:18:14 +0300

    Ошибка

--- test/test_edit_contact_data.py
+++ test/test_edit_contact_data.py
@@ -1,17 +1,18 @@
 from model.contact import Contact
-from random import randrange
+import random
 
 
-def test_edit_contact_data(app):
-    if app.contact.count() == 0:
+def test_edit_contact_data(app, db, check_ui):
+    if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="Mikhail"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
+    old_contacts = db.get_contact_list()
     contact = Contact(firstname="New_name1")
-    contact.id = old_contacts[index].id
-    app.contact.edit_contact_by_index(index, contact)
-    new_contacts = app.contact.get_contact_list()
+    contact.id = random.choice(old_contacts).id
+    app.contact.edit_contact_by_index(contact.id, contact)
+    new_contacts = db.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
-    old_contacts[index] = contact
+    contact.id = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
+    if check_ui:
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
+                                                                     key=Contact.id_or_max)

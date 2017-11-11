7b0214c807f101deb13d58d2a59bef6bb12d6113
motekito <motekito@gmail.com>
2017-11-11 17:49:30 +0300

    29

--- test/test_delete_any_group.py
+++ test/test_delete_any_group.py
@@ -1,4 +1,5 @@
 from model.group import Group
+import pytest
 import random
 
 
@@ -6,11 +7,14 @@ def test_delete_any_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="New"))
     old_groups = db.get_group_list()
-    group = random.choice(old_groups)
-    app.group.delete_group_by_id(group.id)
-    new_groups = db.get_group_list()
-    assert len(old_groups) - 1 == len(new_groups)
-    old_groups.remove(group)
-    assert old_groups == new_groups
-    if check_ui:
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with pytest.allure.step('Given a random group from the list'):
+        group = random.choice(old_groups)
+    with pytest.allure.step('When I delete the group %s from the list' % group):
+        app.group.delete_group_by_id(group.id)
+    with pytest.allure.step('Then the new group list is equal to the old list without the deleted group'):
+        new_groups = db.get_group_list()
+        assert len(old_groups) - 1 == len(new_groups)
+        old_groups.remove(group)
+        assert old_groups == new_groups
+        if check_ui:
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
